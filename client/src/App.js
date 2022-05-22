ac4921826a8b0173488db5bc11305cc12e541a53
Ahmed Alsaedi <aalsaedi265@gmail.com>
2022-05-22 16:04:47 -0500

    the routes and componetns of react

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import React,{useState, useEffect} from 'react'
+import './App.css';
+import Calendar from './comp/caldener'
+import Info from './comp/info'
+import List from './comp/list'
+import Time from './comp/time'
+
+function App() {
+  return (
+    <div className="App">
+     
+     <h1>Welcome to my calnder app it is better then a list app but not by much, but better is better</h1>
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/comp/time.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+export default function Time(){
+
+
+    return (
+
+        <div>
+
+            
+        </div>
+    )
+}
